fe0fe73cc7d78986b37b145d09de458458680779
capcup <cem.aydogan@stud.h-da.de>
2020-04-02 15:23:10 +0200

    define next

--- sudoku_main.py
+++ sudoku_main.py
@@ -4,3 +4,12 @@ from load_json import get_puzzle
 if __name__ == "__main__":
     puzzle_json = get_puzzle()
     print(puzzle_json)
+
+''' NEXT
+1. convert json to array 
+2. use numpy 
+3. implement algorithm
+
+...
+
+'''
